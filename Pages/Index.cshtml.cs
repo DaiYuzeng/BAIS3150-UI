b7d58b29485c33741695dab37a0271f775366f39
Eric Dai <cridequeger23@gmail.com>
2024-10-15 10:45:31 -0600

    Initial commit for the UI part of BAIS3150

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BAIS3150Core.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ Pages/TestPage.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class TestPageModel : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["Title"] = "Test Page";
+        }
+    }
+}
